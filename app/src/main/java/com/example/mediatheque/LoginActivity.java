b99c6dc60f9db03e62ed373d4204a67fb0d678bd
mediatheque <pfe.mediatheque@gmail.com>
2021-06-05 08:47:22 +0200

    Getting started with firebase
    Added a functional registration and authentication page

--- app/src/main/java/com/example/mediatheque/LoginActivity.java
+++ app/src/main/java/com/example/mediatheque/LoginActivity.java
@@ -1,33 +1,87 @@
 package com.example.mediatheque;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mediatheque.ui.book.BookFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView register;
+    private EditText editTextMail_LoginPage,editTextPassword_LoginPage;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        register = (TextView) findViewById(R.id.textViewRegister_LoginPage);
-        register.setOnClickListener(this);
+        editTextMail_LoginPage = findViewById(R.id.editTextMail_LoginPage);                                         // initialise editTextMail_LoginPage
+        editTextPassword_LoginPage = findViewById(R.id.editTextPassword_LoginPage);                                 // initialise editTextPassword_LoginPage
+
+        Button buttonLogin = findViewById(R.id.buttonLogin_LoginPage);                                              // initialise buttonLogin_LoginPage
+        buttonLogin.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();                                                                         // initialise instance for the Firebase Authenticator
+
+        TextView textViewRegister_LoginPage = findViewById(R.id.textViewRegister_LoginPage);                        // initialise textViewRegister_LoginPage
+        textViewRegister_LoginPage.setOnClickListener(this);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch (view.getId()){
 
-            /* When the user clicks on the registration button on the home page it sends them to the registration page */
-            case R.id.textViewRegister_LoginPage:
-                Intent registerPage = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(registerPage);
+            case R.id.textViewRegister_LoginPage:                                                                   // when the user clicks on the registration button on the home page
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));               // it sends them to the registration page
+                break;
+
+            case R.id.buttonLogin_LoginPage:                                                                        //  when the user clicks on the login button on the home page
+                buttonLoginClicked();                                                                               // start the login process
                 break;
         }
+    }
+
+    private void buttonLoginClicked() {
+        String email = editTextMail_LoginPage.getText().toString().trim();                                          //  get the value of the email
+        String pwd = editTextPassword_LoginPage.getText().toString().trim();                                        //  get the value of the password
+
+        if (email.isEmpty()){                                                                                       // if the email is empty
+            editTextMail_LoginPage.setError("Email is required.");                                                  // shows an error message
+            editTextMail_LoginPage.requestFocus();                                                                  // focus on the input field email
+            return;
+        }
+
+        if (pwd.isEmpty()){                                                                                         // if the password is empty
+            editTextPassword_LoginPage.setError("Password is required.");                                           // shows an error message
+            editTextPassword_LoginPage.requestFocus();                                                              // focus on the input field password
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(task -> {                                 // connect the user with his email and password
+            if (task.isSuccessful()){                                                                               // when the task was successfully finish
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));                    // redirects the user to the main page
+            }else {                                                                                                 // when the task was failed
+                Toast.makeText(LoginActivity.this,                                                          // show a failure message
+                        "Failed to login, please check your information.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mediatheque/RegisterActivity.java
+++ app/src/main/java/com/example/mediatheque/RegisterActivity.java
@@ -1,9 +1,7 @@
 package com.example.mediatheque;
 
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -12,138 +10,131 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.jetbrains.annotations.NotNull;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String USERNAME_KEY = "username";
-    private static final String EMAIL_KEY = "email";
-    private static final String PWD_KEY = "pwd";
-    private final DocumentReference userDocument = FirebaseFirestore.getInstance().document("userCollection/user");
+    private static final String USERNAME_KEY = "username";                                                          // name of the USERNAME_KEY column in the table
+    private static final String EMAIL_KEY = "email";                                                                // name of the EMAIL_KEY column in the table
+    private FirebaseFirestore db;                                                                                   // object reference for Firebase Firestore
+    private FirebaseAuth mAuth;                                                                                     // object reference for Firebase Authenticator
 
-    private EditText editTextUsername_RegisterPage,editTextMail_RegisterPage,editTextPassword_RegisterPage,editTextCnfPassword_RegisterPage;
-
-    private Button buttonRegister;
-    private TextView alreadyRegistered;
-
-    FirebaseFirestore db;
+    private EditText editTextUsername_RegisterPage,editTextMail_RegisterPage,
+            editTextPassword_RegisterPage,editTextCnfPassword_RegisterPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();                                                                       // initialise instance for the Firebase Firestore
+        mAuth = FirebaseAuth.getInstance();                                                                         // initialise instance for the Firebase Authenticator
 
-        editTextUsername_RegisterPage = (EditText)findViewById(R.id.editTextUsername_RegisterPage);
-        editTextMail_RegisterPage = (EditText)findViewById(R.id.editTextMail_RegisterPage);
-        editTextPassword_RegisterPage = (EditText)findViewById(R.id.editTextPassword_RegisterPage);
-        editTextCnfPassword_RegisterPage = (EditText)findViewById(R.id.editTextCnfPassword_RegisterPage);
+        editTextUsername_RegisterPage = findViewById(R.id.editTextUsername_RegisterPage);                           // initialise editTextUsername_RegisterPage
+        editTextMail_RegisterPage = findViewById(R.id.editTextMail_RegisterPage);                                   // initialise editTextMail_RegisterPage
+        editTextPassword_RegisterPage = findViewById(R.id.editTextPassword_RegisterPage);                           // initialise editTextPassword_RegisterPage
+        editTextCnfPassword_RegisterPage = findViewById(R.id.editTextCnfPassword_RegisterPage);                     // initialise editTextCnfPassword_RegisterPage
 
-        buttonRegister = (Button) findViewById(R.id.buttonRegister_RegisterPage);
+        Button buttonRegister = findViewById(R.id.buttonRegister_RegisterPage);                                     // initialise buttonRegister_RegisterPage
         buttonRegister.setOnClickListener(this);
 
-        alreadyRegistered = (TextView) findViewById(R.id.textViewLogin_RegisterPage);
+        TextView alreadyRegistered = findViewById(R.id.textViewLogin_RegisterPage);                                 // initialise textViewLogin_RegisterPage
         alreadyRegistered.setOnClickListener(this);
+
+        /*
+        if (mAuth.getCurrentUser() != null){                                                                        // if user is already login go directly to the main page
+            Intent mainPage = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(mainPage);
+        }
+        */
 
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
 
-            /* When the user clicks on the already registered button on the register page it sends them to the home page */
-            case R.id.textViewLogin_RegisterPage:
-                Intent loginPage = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(loginPage);
+            case R.id.textViewLogin_RegisterPage:                                                                   // when the user clicks on the already registered button on the register page
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 break;
 
-            /* When the user clicks on the register button */
-            case R.id.buttonRegister_RegisterPage:
-                buttonRegisterClicked();
+            case R.id.buttonRegister_RegisterPage:                                                                  // when the user clicks on the register button
+                buttonRegisterClicked();                                                                            // start the registration process
                 break;
         }
     }
 
     private void buttonRegisterClicked() {
-        String username = editTextUsername_RegisterPage.getText().toString().trim();                                 //  get the value of the username
-        String email = editTextMail_RegisterPage.getText().toString().trim();                                        //  get the value of the email
-        String pwd = editTextPassword_RegisterPage.getText().toString().trim();                                      //  get the value of the password
-        String cnf_pwd = editTextCnfPassword_RegisterPage.getText().toString().trim();                               //  get the value of the confirmation password
+        String username = editTextUsername_RegisterPage.getText().toString().trim();                                //  get the value of the username
+        String email = editTextMail_RegisterPage.getText().toString().trim();                                       //  get the value of the email
+        String pwd = editTextPassword_RegisterPage.getText().toString().trim();                                     //  get the value of the password
+        String cnf_pwd = editTextCnfPassword_RegisterPage.getText().toString().trim();                              //  get the value of the confirmation password
 
-        /* If the username is empty */
-        if (username.isEmpty()){
-            editTextUsername_RegisterPage.setError("Username is required.");                                         // shows an error message
-            editTextUsername_RegisterPage.requestFocus();                                                            // focus on the input field username
+        if (username.isEmpty()){                                                                                    // if the username is empty
+            editTextUsername_RegisterPage.setError("Username is required.");                                        // shows an error message
+            editTextUsername_RegisterPage.requestFocus();                                                           // focus on the input field username
             return;
         }
 
-        /* If the email is empty */
-        if (email.isEmpty()){
+        if (email.isEmpty()){                                                                                        // if the email is empty
             editTextMail_RegisterPage.setError("Email is required.");                                                // shows an error message
             editTextMail_RegisterPage.requestFocus();                                                                // focus on the input field email
             return;
         }
 
-        /* If the email not match with the structure of a email */
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){                                                       // if the email not match with the structure of a email
             editTextMail_RegisterPage.setError("Please provide valid email.");                                       // shows an error message
             editTextMail_RegisterPage.requestFocus();                                                                // focus on the input field email
             return;
         }
 
-        /* If the password is empty */
-        if (pwd.isEmpty()){
+        if (pwd.isEmpty()){                                                                                          // if the password is empty
             editTextPassword_RegisterPage.setError("Password is required.");                                         // shows an error message
             editTextPassword_RegisterPage.requestFocus();                                                            // focus on the input field password
             return;
         }
 
-        /* If the password lower than 8 */
-        if (pwd.length()<8){
+        if (pwd.length()<8){                                                                                         // if the password lower than 8
             editTextPassword_RegisterPage.setError("Give a password with more than 8 characters.");                  // shows an error message
             editTextPassword_RegisterPage.requestFocus();                                                            // focus on the input field password
             return;
         }
 
-        /* If the confirmation password is empty */
-        if (cnf_pwd.isEmpty()){
+        if (cnf_pwd.isEmpty()){                                                                                      // if the confirmation password is empty
             editTextCnfPassword_RegisterPage.setError("Please reconfirm your password.");                            // shows an error message
             editTextCnfPassword_RegisterPage.requestFocus();                                                         // focus on the input field password
             return;
         }
 
-        /* If the passwords do not match */
-        if (!pwd.equals(cnf_pwd)){
+        if (!pwd.equals(cnf_pwd)){                                                                                   // if the passwords do not match
             editTextCnfPassword_RegisterPage.setError("Password is not matching.");                                  // shows an error message
             editTextCnfPassword_RegisterPage.requestFocus();                                                         // focus on the input field confirmation password
             return;
         }
 
-        Map<String,Object> userCollection = new HashMap<String, Object>();
-        userCollection.put(USERNAME_KEY,username);
-        userCollection.put(EMAIL_KEY,email);
-        userCollection.put(PWD_KEY,pwd);
+        mAuth.createUserWithEmailAndPassword(email,pwd).
+                addOnCompleteListener(task -> {                                                                         // create user with him email and password
+                    Map<String,Object> userCollection = new HashMap<>();                                                // hashmap will save the name of the fields on the database
+                    userCollection.put(USERNAME_KEY,username);                                                          // put the value of username in the USERNAME_KEY column
+                    userCollection.put(EMAIL_KEY,email);                                                                // put the value of email in the EMAIL_KEY column
 
-        userDocument.set(userCollection).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                Toast.makeText(RegisterActivity.this,"Successful",Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull @NotNull Exception e) {
-                Toast.makeText(RegisterActivity.this,"Failed",Toast.LENGTH_SHORT).show();
-            }
-        });
+                    db.collection("userCollection").add(userCollection)                                    // in database adds collection (table) named userCollection
+                            .addOnSuccessListener(documentReference -> {                                                // when the task was successfully finish
+                                Toast.makeText(RegisterActivity.this,                                           // show a successfully message
+                                        "User has been registered successfully.",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));    // redirects the user to the login page
+
+                            }).addOnFailureListener(documentReference -> {                                              // when the task was failed
+                        Toast.makeText(RegisterActivity.this,                                                   // show a failure message
+                                "Registration failed - Connection error with the database.",
+                                Toast.LENGTH_SHORT).show();
+                    });
+                });
+        
     }
 }
